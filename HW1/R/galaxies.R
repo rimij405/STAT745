3f838b357f6cc3447a92bbde50b1206191c38b26
Ian Effendi <effendiian@gmail.com>
2021-09-14 01:15:57 -0400

    Load data in and update project

---
+++ HW1/R/galaxies.R
@@ -0,0 +1,14 @@
+#' Velocities and distances for 24 galaxies in relation to Earth.
+#'
+#' A selection of 24 galaxies from the *New General Catalogue of Nebulae and
+#' Clusters of Stars* (NGC) and the supplemental *Index Catalogues* (IC). Data
+#' provided by course instructor for STAT 745.
+#'
+#' @format A data frame with 24 rows and 3 variables:
+#' \describe{
+#'      \item{Galaxy}{unique id, NGC/IC}
+#'      \item{velocity}{velocity away from Earth, in km per second}
+#'      \item{distance}{distance from Earth, in megaparsecs (3.086 x 10^19 km)}
+#' }
+#' @source Peter Bajorski, RIT 2021
+"galaxies"
